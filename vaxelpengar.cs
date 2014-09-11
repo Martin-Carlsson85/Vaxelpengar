cb7dff70c5701af09ef6d989e09671db3e759a85
Martin <mc22dg@student.lnu.se>
2014-09-11 11:50:44 +0200

    Kompletterat up.

--- vaxelpengar.cs
+++ vaxelpengar.cs
@@ -16,13 +16,13 @@ namespace vaxelpengar
             uint toPay = 0; //Att betala (nr.3)
             double round = 0; //Öresavrundningen (nr.2)
             uint back = 0; //Växel att få tillbaka (nr.5)
-            uint amount500notes;
-            uint amount100notes;
-            uint amount50notes;
-            uint amount20notes;
-            uint amount10coins;
-            uint amount5coins;
-            uint amount1coins;
+            uint amount500notes = 0;
+            uint amount100notes = 0;
+            uint amount50notes = 0;
+            uint amount20notes = 0;
+            uint amount10coins = 0;
+            uint amount5coins = 0;
+            uint amount1coins = 0;
 
 
             // while-loop
@@ -32,7 +32,17 @@ namespace vaxelpengar
                 {   //Försöker översätta inmatat värde till en double-variabel.
                     Console.Write("Ange totalsumman:");
                     total = double.Parse(Console.ReadLine());
+                    
+
+                    //Gör så att programmet avslutas vid totaltsumma mindre än 1 krona. 
+                    if (total < 1)
+                    {
+
+                        throw new Exception();
+
+                    }
                     break;
+
                 }
                 catch
                 {   //Ifall det inte fungerade så kommer detta att köras istället.
@@ -42,6 +52,11 @@ namespace vaxelpengar
                 }
             }
 
+            //Öresavrundningsmetod
+            toPay = (uint)Math.Round(total);
+
+       
+
             while (true)
             {
                 try
@@ -49,6 +64,7 @@ namespace vaxelpengar
                     Console.Write("Ange erhållet belopp:");
                     sum = uint.Parse(Console.ReadLine());
                     break;
+
                 }
                 catch
                 {   //Ifall det inte fungerade så kommer detta att köras istället.
@@ -59,38 +75,17 @@ namespace vaxelpengar
 
             }
 
-            //Öresavrundningsmetod
-            toPay = (uint)Math.Round(total);
 
+            //Om kunden betalar för lite pengar säger programmet ifrån (det blir fel). Denna funktion "Kastar" felet.
 
-            //while-loop
-            while (true)
+            if (sum < toPay)
             {
-                try
-                {
-                    //Om kunden betalar för lite pengar säger programmet ifrån (det blir fel). Denna funktion "Kastar" felet.
-
-                    if (sum <= toPay)
-                    {
-                        throw new Exception();
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                catch
-                {   
-                    //Här fångas felet upp och felmeddelande visas.
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nErhållet belopp är för litet.");
-                    Console.ResetColor();
-                    Console.Write("Ange erhållet belopp:");
-                    sum = uint.Parse(Console.ReadLine());
-                    
-                }
-
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nErhållet belopp är för litet.");
+                Console.ResetColor();
+                return;
             }
+
             //Här räknas öresavrudningen fram.
             round = toPay - total;
 
@@ -102,7 +97,7 @@ namespace vaxelpengar
 
             //if-satser som konstrollerar det kunden ska ha tillbaka. Växeln (back) delas med valören, 
             //blir svaret högre än 1 så går programmet vidare.
-            
+
             if (back / 500 >= 1)
             {
                 amount500notes = back / 500;
@@ -149,11 +144,50 @@ namespace vaxelpengar
 
             //Kvitto
             Console.WriteLine("\nKVITTO");
+            Console.WriteLine("----------------------");
             Console.WriteLine("Totalt: {0}", total);
             Console.WriteLine("Öresavrudning: {0}", round);
             Console.WriteLine("Att betala: {0}", toPay);
             Console.WriteLine("Kontant: {0}", sum);
             Console.WriteLine("Tillbaka: {0}", backFixed);
+            Console.WriteLine("----------------------");
+
+            //Här presnteras antalet sedlar som kunden får tillbaka.
+            if (amount500notes != 0)
+            {
+                Console.WriteLine("500-lappar: {0}", amount500notes);
+            }
+
+            if (amount100notes != 0)
+            {
+                Console.WriteLine("100-lappar: {0}", amount100notes);
+            }
+
+            if (amount50notes != 0)
+            {
+                Console.WriteLine("50-lappar: {0}", amount50notes);
+            }
+
+            if (amount20notes !=0)
+            {
+                Console.WriteLine("20-lappar: {0}", amount20notes);
+            }
+            
+            if (amount10coins !=0)
+            {  
+                Console.WriteLine("5-kronor: {0}", amount10coins);
+            }
+            
+            if (amount5coins !=0)
+            {
+                Console.WriteLine("5-kronor: {0}", amount5coins);
+            }
+            
+            if (amount1coins !=0)
+            {
+                Console.WriteLine("1-kronor: {0}", amount1coins);
+            }
+
 
         }
     }
